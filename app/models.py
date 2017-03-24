21a4ae3602aa2f36ddae2985c8f0a655a5e14247
Kate <kate.glebova97@gmail.com>
2017-03-24 12:38:58 +0200

    Create models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,50 @@
-from run import db
+from app import db
+
 
 class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    vacancies = db.relationship('Vacancy', backref='department')
+    employees = db.relationship('Employee', backref='department')
+
+
+class Position(db.Model):
+    __tablename__ = 'positions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    vacancies = db.relationship('Vacancy', backref='position')
+    employees = db.relationship('Employee', backref='position')
+
+
+class Vacancy(db.Model):
+    __tablename__ = 'vacancies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    publishment_date = db.Column(db.Date, nullable=False)
+    closing_date = db.Column(db.Date, nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
+    position_id = db.Column(db.Integer, db.ForeignKey('positions.id'), nullable=False)
+    employee = db.relationship('Employee', uselist=False, back_populates='vacancies')
+
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    surname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    is_director = db.Column(db.Boolean, default=False)
+    is_fired = db.Column(db.Boolean, default=False)
+    vacancy_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
+    position_id = db.Column(db.Integer, db.ForeignKey('positions.id'), nullable=False)
+    vacancy = db.relationship('Vacancy', back_populates='employees')
--- config.py
+++ config.py
@@ -5,4 +5,5 @@ base_dir = os.path.abspath('./')
 DEBUG = True
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = base_dir + 'db/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'test.sqlite')
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
