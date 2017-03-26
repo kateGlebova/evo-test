248b69a9a688d18fe5904c6ad0ff6c0d7ea3e30f
Kate <kate.glebova97@gmail.com>
2017-03-27 02:01:41 +0300

    Fix rendering error templates

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -5,9 +5,9 @@ from app import app
 
 @app.errorhandler(404)
 def not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
