23fd6efe1aeda9a651f8494e75c5d4daaf3259a2
Kate <kate.glebova97@gmail.com>
2017-03-27 04:44:39 +0300

    Back to sqlite

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ base_dir = os.path.abspath(os.path.dirname(__file__) )
 DEBUG = True
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'test.sqlite')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'hr.db')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SECRET_KEY = 'kjghs8xhtkqi4*yr87+rv@7#+y4njwrd4+9v)a*+ztduw#9q^s&jv+zgaeg'
